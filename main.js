02ca5216e083730d20b2aef99f058ea462711d24
PedroHenriqueDEV26 <112581681+PedroHenriqueDEV26@users.noreply.github.com>
2024-07-24 12:08:19 -0300

    aula 136

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
+var vid = ""
+var status = ""
 function preload(){
-
+    vid = createVideo("video.mp4")
+    vid.hide()
 }
 
 function setup(){
-canvas = createCanvas(600, 500) 
-canvas.center()
+    canvas = createCanvas(600, 500) 
+    canvas.center()
+
 }
 
 function draw(){
-    
+    image(vid,0, 0, 600, 500)
 }
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Objeto Detectado"
+}
+
+function modelLoaded(){
+    console.log("modelo carregado!")
+    status = true
+    vid.loop()
+    vid.speed(1.2)
+    vid.volume(0)
+}
+
